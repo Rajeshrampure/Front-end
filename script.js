4cb573d498889ed831302e33d3da47a9be81cef3
Rajeshrampure <rajeshrampure1997@gmail.com>
2022-06-09 20:58:16 +0530

    INITIAL

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("Hello web")
+document.getElementById("card").style.display="none" //remove particular item from the browser
+document.getElementById("card").style.visibility="hidden" //space will remain in browser
+
+let res = document.getElementById("card");
+res.style.backgroundColor = 'red';
